472fd0aafe5ae55b027fa57ed95772a72037d4d1
Sailab12345 <sailabbasak1998@gmail.com>
2021-03-16 16:13:28 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -1,49 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import ToDo from './todo';
-import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
-import {Provider,connect} from 'react-redux';
+import ToDo from "./todo";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
 
-
-
-const counter=(state=0, action)=>{
-  switch(action.type){
-    case 'INCREMENT':
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
       return state + 1;
-    case 'DECREMENT':
+    case "DECREMENT":
       return state - 1;
     default:
       return state;
   }
-} 
-const store= createStore(counter);
-const ToDoMain=(props)=>(
+};
+const store = createStore(counter);
+const ToDoMain = (props) => (
   <div>
     <ToDo
       value={props.state}
-      onIncrement={()=>
-                    store.dispatch({
-                      type:'INCREMENT'
-                    })
-                  }
-      
-      onDecrement={()=>
-                    store.dispatch({
-                      type:'DECREMENT'
-                    })
-                  }
+      onIncrement={() =>
+        store.dispatch({
+          type: "INCREMENT"
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: "DECREMENT"
+        })
+      }
     />
   </div>
-
 );
-const mapStateToProps=(state)=>({state:state});
-const MainComponent= connect(mapStateToProps)(ToDoMain);
+const mapStateToProps = (state) => ({ state: state });
+const MainComponent = connect(mapStateToProps)(ToDoMain);
 
 ReactDOM.render(
   <Provider store={store}>
-    <MainComponent/>
+    <MainComponent />
   </Provider>,
   document.getElementById("root")
 );
