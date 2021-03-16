59d034dbb52832b68e456579aca91f6ec3f8b3bd
trdp30 <trdp30@gmail.com>
2021-03-16 09:28:23 +0530

    code refactored

--- src/index.js
+++ src/index.js
@@ -1,49 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import ToDo from './todo';
-import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import ToDo from "./todo";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
 
-
-
-const counter=(state=0, action)=>{
-  switch(action.type){
-    case 'INCREMENT':
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
       return state + 1;
-    case 'DECREMENT':
+    case "DECREMENT":
       return state - 1;
     default:
       return state;
   }
-} 
-const store= createStore(counter);
-const ToDoMain=()=>(
+};
+const store = createStore(counter);
+
+const ToDoMain = (props) => (
   <div>
     <ToDo
-      value={store.getState()}
-      onIncrement={()=>
-                    store.dispatch({
-                      type:'INCREMENT'
-                    })
-                  }
-      
-      onDecrement={()=>
-                    store.dispatch({
-                      type:'DECREMENT'
-                    })
-                  }
+      value={props.state}
+      onIncrement={() => {
+        store.dispatch({
+          type: "INCREMENT"
+        });
+      }}
+      onDecrement={() =>
+        store.dispatch({
+          type: "DECREMENT"
+        })
+      }
     />
   </div>
+);
 
-)
+const mapStateToProps = (state) => ({ state: state });
+
+const MainComponent = connect(mapStateToProps)(ToDoMain);
 
 ReactDOM.render(
-  <Provider store={createStore(ToDoMain)}>
-    <ToDoMain/>
+  <Provider store={store}>
+    <MainComponent />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
